ce5b38fc8a9fc3dde3f4d080e45900e38be9f017
Sandy McMillan <sandy_mcmillan@yahoo.co.uk>
2020-11-26 11:48:55 +0000

    another test passing

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,5 +1,7 @@
 def fizz_buzz(number):
     if number == 3:
         return "fizz"
-    else:
+    elif number == 5:
         return "buzz"
+    else:
+        return "fizzbuzz"
--- tests/fizz_buzz_test.py
+++ tests/fizz_buzz_test.py
@@ -9,3 +9,6 @@ class TestFizzBuzz(unittest.TestCase):
 
     def test_fizz_buzz__5_returns_buzz(self):
         self.assertEqual("buzz", fizz_buzz(5))
+    
+    def test_fizz_buzz__15_returns_fizzbuzz(self):
+        self.assertEqual("fizzbuzz", fizz_buzz(15))
