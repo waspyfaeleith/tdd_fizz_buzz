effa9df5d0f118decc3a8ebaa162be0172449ca7
Sandy McMillan <sandy_mcmillan@yahoo.co.uk>
2020-11-26 12:01:10 +0000

    fixed for divisible by 5

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,9 +1,9 @@
 def fizz_buzz(number):
-    if number == 3:
-        return "fizz"
-    elif number == 5:
-        return "buzz"
-    elif number == 15:
+    if number == 15:
         return "fizzbuzz"
+    elif number % 3 == 0:
+        return "fizz"
+    elif number % 5 == 0:
+        return "buzz"
     else:
         return str(number)
--- tests/fizz_buzz_test.py
+++ tests/fizz_buzz_test.py
@@ -7,8 +7,15 @@ class TestFizzBuzz(unittest.TestCase):
     def test_fizz_buzz__3_returns_fizz(self):
        self.assertEqual("fizz", fizz_buzz(3)) 
 
+
+    def test_fizz_buzz__6_returns_fizz(self):
+        self.assertEqual("fizz", fizz_buzz(6))
+
     def test_fizz_buzz__5_returns_buzz(self):
         self.assertEqual("buzz", fizz_buzz(5))
+
+    def test_fizz_buzz__10_returns_buzz(self):
+        self.assertEqual("buzz", fizz_buzz(10))
     
     def test_fizz_buzz__15_returns_fizzbuzz(self):
         self.assertEqual("fizzbuzz", fizz_buzz(15))
